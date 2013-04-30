0e39d74d3a9ef9c288193f21245405d755a5dd29
meniwohli <mmenhard@tsn.at>
2013-04-30 13:45:25 +0200

    Reservierungsanzeige und Reservierungen löschen

---
+++ php/reservierungloeschen.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<?php
+
+	include "includeup.php";
+	
+	
+	//Wenn eingeloggt und Benutzerrecht weiter..
+	if (isset($_SESSION["login"]) && $_SESSION["login"] == "ok") 
+	{
+		
+		
+		//Reservierung ID's mit POST vergleichen
+		$reservierungen = $sql->arrayCall("SELECT Reservierung_ID FROM tb_reservierung");
+		
+		$rid = true;
+		
+		foreach($reservierungen as $r)
+		{
+			$id = $r["Reservierung_ID"];
+			
+			if(isset($_POST["$id"]))
+			{
+				$rid = $id;
+			}
+		}
+		
+		if(isset($_POST["bestaetigt"]))
+			{
+				$sql->change("DELETE FROM tb_reservierung WHERE Reservierung_ID = $rid");
+				$daten["geloescht"] = true;
+			}
+		
+		$daten["rid"] = $rid;
+		
+		
+		
+		
+		
+		//auf Template verweisen
+		$template = $twig->loadTemplate('reservierungloeschen.twig');
+		
+	//sonst auf anmeldeseite
+	}
+	
+	
+	include "includedown.php";
+
+?>
