e12ec8c22c7317f6bc1f4210958c3cad2089ace6
meniwohli <mmenhard@tsn.at>
2013-05-02 23:33:37 +0200

    farben update

--- php/farben.php
+++ php/farben.php
@@ -6,7 +6,7 @@
 	//Wenn eingeloggt, weiter..
 	if (isset($_SESSION["login"]) && $_SESSION["login"] == "ok") { 
 	
-		
+		//alle Farben übergeben
 		$speicher = $sql->arrayCall("SELECT * FROM tb_farben");
 		$farben = array();
 		
@@ -15,16 +15,43 @@
 			$farben[$x['FarbCode']] = $x['FarbName'];
 		}
 		
-		
-		
-		
-		
-		
-		
+		//farbzuweisung auslesen und übergeben
+		$speicher = $sql->arrayCall("SELECT f.FarbCode, f.FarbName, fz.Reservierungsart FROM tb_farben as f, 
+									tb_farbzuweisung as fz WHERE f.Farbe_ID = fz.fk_Farbe_ID");
+		//array durchlaufen und einzelne werte zuweisen
+		$farbenaktuell;
+		foreach($speicher as $x)
+		{
+			if($x["Reservierungsart"] == "frei")
+			{
+				$farbenaktuell[$x["Reservierungsart"]] = $x["FarbCode"];
+			}
+			
+			if($x["Reservierungsart"] == "besetzt")
+			{
+				$farbenaktuell[$x["Reservierungsart"]] = $x["FarbCode"];
+			}
+			
+			if($x["Reservierungsart"] == "zu")
+			{
+				$farbenaktuell[$x["Reservierungsart"]] = $x["FarbCode"];
+			}
+			
+			if($x["Reservierungsart"] == "serie")
+			{
+				$farbenaktuell[$x["Reservierungsart"]] = $x["FarbCode"];
+			}
+			
+			if($x["Reservierungsart"] == "turnier")
+			{
+				$farbenaktuell[$x["Reservierungsart"]] = $x["FarbCode"];
+			}
+		}
 		
 		
 		//übergabe
 		$daten["farbarray"] = $farben;
+		$daten["farbenaktuell"] = $farbenaktuell;
 		
 		
 		//auf Template verweisen
