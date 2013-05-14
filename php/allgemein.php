23d70017b6639e0cc7948ae224984984b6569cd3
meniwohli <mmenhard@tsn.at>
2013-05-14 14:13:56 +0200

    allgemein öffnungszeiten übergeben

--- php/allgemein.php
+++ php/allgemein.php
@@ -8,6 +8,10 @@
 	
 		
 		
+		//Liest die Öffnungszeiten aus
+		$speicher = $sql->call("SELECT * FROM tb_zeiten");
+		$zeit["offenVon"] = $speicher["OffenVon"];
+		$zeit["offenBis"] = $speicher["OffenBis"];
 		
 		
 		
@@ -22,6 +26,7 @@
 		
 		
 		
+		$daten["zeit"] = $zeit;
 		
 		//auf Template verweisen
 		$template = $twig->loadTemplate('allgemein.twig');
