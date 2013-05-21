c4496ccb14556a8aabb9e69013fb0cb6356104c0
meniwohli <mmenhard@tsn.at>
2013-05-21 12:21:53 +0200

    allgemein funktionsfähig

--- php/allgemein.php
+++ php/allgemein.php
@@ -17,9 +17,33 @@
 		
 		
 		
+		if(isset($_POST["offenvon"]) && ($_POST["offenvon"] != $speicher["OffenVon"]) )
+		{
+			$x = $_POST["offenvon"];
+			$sql->change("UPDATE tb_zeiten SET OffenVon = $x");
+			$zeit["offenVon"] = $_POST["offenvon"];
+		}
 		
+		if(isset($_POST["offenbis"]) && ($_POST["offenbis"] != $speicher["OffenBis"]) )
+		{
+			$x = $_POST["offenbis"];
+			$sql->change("UPDATE tb_zeiten SET OffenBis = $x");
+			$zeit["offenBis"] = $_POST["offenbis"];
+		}
 		
+		if(isset($_POST["saisonvon"]) && ($_POST["saisonvon"] != $speicher["SaisonVon"]) )
+		{
+			$x = $_POST["saisonvon"];
+			$sql->change("UPDATE tb_zeiten SET SaisonVon = '$x'");
+			$zeit["saisonvon"] = $_POST["saisonvon"];
+		}
 		
+		if(isset($_POST["saisonbis"]) && ($_POST["saisonbis"] != $speicher["SaisonBis"]) )
+		{
+			$x = $_POST["saisonbis"];
+			$sql->change("UPDATE tb_zeiten SET SaisonBis = '$x'");
+			$zeit["saisonbis"] = $_POST["saisonbis"];
+		}
 		
 		
 		
