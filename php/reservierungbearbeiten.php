f5e03093b9daa80d9aed33eb7044a74fbe3c7d1b
meniwohli <mmenhard@tsn.at>
2013-06-05 20:16:26 +0200

    Reservierung bearbeiten Anzeige passt Verarbeitung fehlt noch

--- php/reservierungbearbeiten.php
+++ php/reservierungbearbeiten.php
@@ -9,13 +9,43 @@
 	if (isset($_SESSION["login"]) && $_SESSION["login"] == "ok") { 
 		
 		
+		if(isset($_POST["bearbeitet"]))
+		{
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			$sql->change("");
+			$daten["bearbeitet"] = true;
+		}
+		
+		
+		
+		$rid = $_POST["resbearb"];
+		
+		$reservierung = $sql->call("SELECT * FROM tb_reservierung WHERE Reservierung_ID = '$rid'");
+		
+		$zeit = $sql->call("SELECT * FROM tb_zeiten");
+			
+		$mitglieder = $sql->arrayCall("SELECT * FROM tb_mitglied");
+			
+		$art = $sql->arrayCall("SELECT * FROM tb_reservierungsart");
 		
 		
 		
 		
 		
 		
-		
+		$daten["zeit"]=$zeit;
+		$daten["res"]=$reservierung;
+		$daten["mitglieder"]=$mitglieder;
+		$daten['art']=$art;
 		
 		
 		
